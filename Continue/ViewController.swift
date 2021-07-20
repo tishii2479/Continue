3cf1e43842b984488342664bac3889a9dd49f425
Tatsuya Ishii <tishii2479@gmail.com>
2021-07-20 22:56:38 +0900

    Initial commit

---
+++ Continue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Continue
+//
+//  Created by Tatsuya Ishii on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
